e5b531f9355384922820c39eb28a985a5424b5a6
DilipSingh004 <singhdilip9833@gmail.com>
2025-06-21 15:20:07 +0530

    Create Contact.jsx

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+
+function Contact() {
+  return (
+    <Layout>
+    <div>
+      
+    </div>
+    </Layout>
+  )
+}
+
+export default Contact
