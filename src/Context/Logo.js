38b88569cac4dc2f367912b711268b213df61fc4
DilipSingh004 <singhdilip9833@gmail.com>
2025-07-09 14:09:41 +0530

    Update Logo.js

--- src/Context/Logo.js
+++ src/Context/Logo.js
@@ -1,82 +1,43 @@
+import AsusLogo from '../Assets/Logo/AsusLogo.jpg';
+import JBLLogo from '../Assets/Logo/JBLLogo.jpg';
+import HPLogo from '../Assets/Logo/HPLogo.jpg';
+import DysonLogo from '../Assets/Logo/DysonLogo.jpg';
+import LenovoLogo from '../Assets/Logo/LenovoLogo.jpg';
+import LogitechLogo from '../Assets/Logo/LogitechLogo.jpg';
+import XboxLogo from '../Assets/Logo/XboxLogo.jpg';
+import SonyLogo from '../Assets/Logo/SonyLogo.jpeg';
+import PlayStationLogo from '../Assets/Logo/PlayStationLogo.jpg';
+import PnyLogo from '../Assets/Logo/PnyLogo.jpg';
+import MSILogo from '../Assets/Logo/MSI-Logo.png';
+import NintendoLogo from '../Assets/Logo/NintendoLogo.jpg';
+import AMDLogo from '../Assets/Logo/AMDLogo.jpeg';
+import AcerLogo from '../Assets/Logo/AcerLogo.jpeg';
+import AppleLogo from '../Assets/Logo/AppleLogo.png';
+import DellLogo from '../Assets/Logo/DellLogo.jpeg';
+import HarmonKardonLogo from '../Assets/Logo/HarmonKardonLogo.jpeg';
+import IntelLogo from '../Assets/Logo/IntelLogo.jpg';
+import NvidiaLogo from '../Assets/Logo/NvidiaLogo.jpg';
+import ToshibaLogo from '../Assets/Logo/ToshibaLogo.jpeg';
+
 export const Logo = [
-  {
-    id:1,
-    image: require('../Assets/Logo/AsusLogo.jpg')
-  },
-  {
-    id:2,
-    image: require('../Assets/Logo/JBLLogo.jpg')
-  },
-  {
-    id:3,
-    image: require('../Assets/Logo/HPLogo.jpg')
-  },
-  {
-    id:4,
-    image: require('../Assets/Logo/DysonLogo.jpg')
-  },
-  {
-    id:5,
-    image: require('../Assets/Logo/LenovoLogo.jpg')
-  },
-  {
-    id:6,
-    image: require('../Assets/Logo/LogitechLogo.jpg')
-  },
-  {
-    id:7,
-    image: require('../Assets/Logo/XboxLogo.jpg')
-  },
-  {
-    id:8,
-    image: require('../Assets/Logo/SonyLogo.jpeg')
-  },
-  {
-    id:9,
-    image: require('../Assets/Logo/PlaystationLogo.jpg')
-  },
-  {
-    id:10,
-    image: require('../Assets/Logo/PnyLogo.jpg')
-  },
-   {
-    id:11,
-    image: require('../Assets/Logo/MSI-Logo.png')
-  },
-  {
-    id:12,
-    image: require('../Assets/Logo/NintendoLogo.jpg')
-  },
-  {
-    id:13,
-    image: require('../Assets/Logo/AMDLogo.jpeg')
-  },
-  {
-    id:14,
-    image: require('../Assets/Logo/AcerLogo.jpeg')
-  },
-  {
-    id:15,
-    image: require('../Assets/Logo/AppleLogo.png')
-  },
-  {
-    id:16,
-    image: require('../Assets/Logo/DellLogo.jpeg')
-  },
-  {
-    id:17,
-    image: require('../Assets/Logo/HarmonKardonLogo.jpeg')
-  },
-  {
-    id:18,
-    image: require('../Assets/Logo/IntelLogo.jpg')
-  },
-  {
-    id:19,
-    image: require('../Assets/Logo/NvidiaLogo.jpg')
-  },
-  {
-    id:20,
-    image: require('../Assets/Logo/ToshibaLogo.jpeg')
-  },
+  { id: 1, image: AsusLogo },
+  { id: 2, image: JBLLogo },
+  { id: 3, image: HPLogo },
+  { id: 4, image: DysonLogo },
+  { id: 5, image: LenovoLogo },
+  { id: 6, image: LogitechLogo },
+  { id: 7, image: XboxLogo },
+  { id: 8, image: SonyLogo },
+  { id: 9, image: PlayStationLogo },
+  { id: 10, image: PnyLogo },
+  { id: 11, image: MSILogo },
+  { id: 12, image: NintendoLogo },
+  { id: 13, image: AMDLogo },
+  { id: 14, image: AcerLogo },
+  { id: 15, image: AppleLogo },
+  { id: 16, image: DellLogo },
+  { id: 17, image: HarmonKardonLogo },
+  { id: 18, image: IntelLogo },
+  { id: 19, image: NvidiaLogo },
+  { id: 20, image: ToshibaLogo },
 ];
