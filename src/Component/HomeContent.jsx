7d84c71808ec2649ea961db641f893e9ae27301f
DilipSingh004 <singhdilip9833@gmail.com>
2025-07-12 15:01:22 +0530

    Update HomeContent.jsx

--- src/Component/HomeContent.jsx
+++ src/Component/HomeContent.jsx
@@ -40,14 +40,14 @@ function HomeContent() {
     <div className="bg-white dark:bg-gray-950 mb-auto transition-colors duration-500">
       <motion.section
         ref={ref}
-        className="flex items-center justify-center px-6 pb-10  pt-28"
+        className="flex items-center justify-center px-6 pb-10  pt-24"
         variants={containerVariants}
         initial="hidden"
         animate={controls}
       >
         <motion.div className="max-w-4xl text-center">
           <motion.div
-            className="inline-flex items-center gap-2 bg-gray-100 dark:bg-gray-800 px-4 py-1 rounded-full text-sm text-gray-700 dark:text-gray-300 mb-6"
+            className="inline-flex items-center gap-2 bg-gray-100 dark:bg-gray-800 px-4 py-1 rounded-full text-sm text-gray-700 dark:text-gray-300 mb-2"
             variants={fadeInUp}
           >
             <FaCheckCircle className="text-blue-500" />
@@ -55,7 +55,7 @@ function HomeContent() {
           </motion.div>
 
           <motion.h1
-            className="text-3xl sm:text-4xl md:text-4xl font-bold tracking-tight text-gray-900 dark:text-white"
+            className="text-2xl sm:text-3xl md:text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
             variants={fadeInUp}
           >
             Fawtech Electronics is UAE's leading electronics store.
@@ -66,7 +66,7 @@ function HomeContent() {
           </motion.h1>
 
           <motion.p
-            className="mt-6 text-lg md:text-xl text-gray-600 dark:text-gray-300 max-w-3xl mx-auto"
+            className="mt-2 text-xs md:text-xl text-gray-600 dark:text-gray-300 max-w-2xl mx-auto"
             variants={fadeInUp}
           >
             One stop destination for all electronics products like Mobiles,
@@ -74,12 +74,12 @@ function HomeContent() {
           </motion.p>
 
           <motion.div
-            className="mt-10 flex flex-col sm:flex-row justify-center items-center gap-4"
+            className="mt-2 flex flex-col sm:flex-row justify-center items-center gap-4"
             variants={fadeInUp}
           >
             <motion.button
               onClick={handleLearnMore}
-              className="relative overflow-hidden flex items-center gap-2 px-6 py-3 bg-gray-900 text-white rounded-full group transition-all duration-300"
+              className="relative overflow-hidden flex items-center gap-2 px-4 py-2 bg-gray-900 text-white rounded-full group transition-all duration-300"
               variants={buttonHover}
               whileHover="hover"
               whileTap="tap"
